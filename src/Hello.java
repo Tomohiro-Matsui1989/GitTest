f431e3a01127a705977d30d0efbb2c95b4995394
ryoga <tech.ryoga@gmail.com>
2025-07-07 16:57:06 +0900

    test2

--- src/Hello.java
+++ src/Hello.java
@@ -6,6 +6,8 @@ public class Hello {
     	System.out.println("22222");
     	System.out.println("todasensei");
     	System.out.println("matsuisensei");
+      	System.out.println("todasensei");
+    	System.out.println("matsuisensei");
     }
 }
 
