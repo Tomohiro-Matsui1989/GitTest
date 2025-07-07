624757875f393334c4c13931540738603031014c
mid <mid@KCSK3241.k302.kcsk.ac.jp>
2025-07-07 16:59:09 +0900

    test3

--- src/Hello.java
+++ src/Hello.java
@@ -8,6 +8,10 @@ public class Hello {
     	System.out.println("matsuisensei");
       	System.out.println("todasensei");
     	System.out.println("matsuisensei");
+    	System.out.println("todasensei");
+    	System.out.println("matsuisensei");
+      	System.out.println("todasensei");
+    	System.out.println("matsuisensei");
     }
 }
 
